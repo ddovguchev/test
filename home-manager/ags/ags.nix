5fe85606f1381feca253aceb70d13e54712c1c20
Dzmitriy Dovguchev <ddovguchev@gmail.com>
2026-02-17 01:50:59 +0300

    fix

--- home-manager/ags/ags.nix
+++ home-manager/ags/ags.nix
@@ -1,5 +1,12 @@
-{ config, pkgs, ... }:
+{ config, pkgs, lib, ... }:
 {
   # Deploy full AGS/Astal config from repo example/ to ~/.config/ags
   xdg.configFile."ags".source = ./config;
+
+  # Typelibs для ags run: gjs ищет AstalWp и др. по GI_TYPELIB_PATH
+  home.sessionVariables.GI_TYPELIB_PATH = lib.makeSearchPath "lib/girepository-1.0" [
+    pkgs.astal.wireplumber
+    pkgs.astal.astal3
+    pkgs.astal.io
+  ];
 }
