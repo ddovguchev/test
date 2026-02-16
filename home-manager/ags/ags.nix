e0c0b71367eb02c08d1b8f18345e5802de829f67
Dzmitriy Dovguchev <ddovguchev@gmail.com>
2026-02-17 02:31:43 +0300

    fix

--- home-manager/ags/ags.nix
+++ home-manager/ags/ags.nix
@@ -1,7 +1,6 @@
-# AGS (Astal): конфиг из examples/ags + обёртки для typelibs.
+# AGS (Astal): конфиг + обёрнутый бинарь с typelibs и GSettings-схемами.
 { config, pkgs, lib, ... }:
 let
-  # Конфиг AGS в этой папке
   agsConfigDir = ./config;
   astalPkgs = with pkgs; [
     astal.astal3
@@ -11,21 +10,23 @@ let
   ];
   typelibPath = lib.makeSearchPath "lib/girepository-1.0" astalPkgs;
   schemaPath = lib.makeSearchPath "share/glib-2.0/schemas" astalPkgs;
-  agsRun = pkgs.writeShellScript "ags-run" ''
-    export GI_TYPELIB_PATH="${typelibPath}"
-    export GSETTINGS_SCHEMA_DIR="${schemaPath}"
-    cd "''${AGS_CONFIG:-$HOME/.config/ags}" && exec ${pkgs.ags}/bin/ags run "$@"
+  dataDirs = lib.makeSearchPath "share" astalPkgs;
+  # Обёрнутый ags: переменные заданы в бинаре, работает при любом вызове (ags run, ags init и т.д.)
+  wrappedAgs = pkgs.runCommand "ags-wrapped" {
+    buildInputs = [ pkgs.makeWrapper ];
+  } ''
+    mkdir -p $out/bin
+    makeWrapper ${pkgs.ags}/bin/ags $out/bin/ags \
+      --set GI_TYPELIB_PATH "${typelibPath}" \
+      --set GSETTINGS_SCHEMA_DIR "${schemaPath}" \
+      --prefix XDG_DATA_DIRS : "${dataDirs}"
   '';
-  # Обёртка ags: "ags run" идёт через ags-run (с typelibs), остальное — в настоящий ags.
-  agsWrapper = pkgs.writeShellScript "ags-wrapper" ''
-    case "''${1:-}" in
-      run) exec "${agsRun}" "''${@:2}" ;;
-      *)   exec ${pkgs.ags}/bin/ags "$@" ;;
-    esac
+  # ags-run: перейти в ~/.config/ags и запустить обёрнутый ags run (для exec-once)
+  agsRun = pkgs.writeShellScript "ags-run" ''
+    cd "''${AGS_CONFIG:-$HOME/.config/ags}" && exec ${wrappedAgs}/bin/ags run "$@"
   '';
 in
 {
-  # Развернуть конфиг AGS в ~/.config/ags
   xdg.configFile."ags".source = agsConfigDir;
 
   home.sessionPath = [ "$HOME/.local/bin" ];
@@ -35,14 +36,15 @@ in
     executable = true;
   };
 
+  # В PATH — обёрнутый ags, чтобы «ags run» в терминале тоже видел схемы
   home.file.".local/bin/ags" = {
-    source = agsWrapper;
+    source = "${wrappedAgs}/bin/ags";
     executable = true;
   };
 
-  # Чтобы ags init и типы работали: GIR для ts-for-gir (если понадобится)
   home.sessionVariables = {
     GI_TYPELIB_PATH = typelibPath;
     GSETTINGS_SCHEMA_DIR = schemaPath;
+    XDG_DATA_DIRS = dataDirs;
   };
 }
