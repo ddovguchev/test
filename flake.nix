f3064e31684ab6b70fb320e5b2fd4a1fb3f74824
Your Name <you@example.com>
2026-02-17 20:05:03 +0300

    fix flake conflict and enable local sddm module

--- flake.nix
+++ flake.nix
@@ -9,45 +9,50 @@
     flake-utils.url = "github:numtide/flake-utils";
   };
   outputs = { self, nixpkgs, home-manager, flake-utils, ... }:
+<<<<<<< Updated upstream
+=======
+    < < < < <<< HEAD
+>>>>>>> Stashed changes
   let
-    system = "x86_64-linux";
-    pkgs = import nixpkgs { inherit system; config.allowUnfree = true; };
+  system = "x86_64-linux";
+  pkgs = import nixpkgs { inherit system; config.allowUnfree = true; };
   in
   {
-    nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
-      inherit system;
-      modules = [
-        ./hardware-configuration.nix
-        ./modules/boot.nix
-        ./modules/networking.nix
-        ./modules/users.nix
-        ./modules/keyboard.nix
-        ./modules/nvidia.nix
-        ./modules/hyprland.nix
-        ./modules/audio.nix
-        ./modules/packages.nix
-        ./sddm/SilentSDDM/nix/module.nix
-        home-manager.nixosModules.home-manager
-        ({ config, pkgs, ... }: {
-          system.stateVersion = "25.11";
-          nixpkgs.config.allowUnfree = true;
-          time.timeZone = "Europe/Minsk";
-          programs.silentSDDM = {
-            enable = true;
-            theme = "rei";
-          };
-          home-manager.useGlobalPkgs = true;
-          home-manager.useUserPackages = true;
-          home-manager.backupFileExtension = "bak";
-          home-manager.users.hikari = { pkgs, ... }: {
-            home.stateVersion = "25.11";
-            programs.home-manager.enable = true;
-            imports = [ ./home-manager/home-manager.nix ];
-          };
-        })
-      ];
-    };
-  } // flake-utils.lib.eachDefaultSystem (system:
+  nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+    inherit system;
+    modules = [
+      ./hardware-configuration.nix
+      ./modules/boot.nix
+      ./modules/networking.nix
+      ./modules/users.nix
+      ./modules/keyboard.nix
+      ./modules/nvidia.nix
+      ./modules/hyprland.nix
+      ./modules/audio.nix
+      ./modules/packages.nix
+      ./sddm/SilentSDDM/nix/module.nix
+      home-manager.nixosModules.home-manager
+      ({ config, pkgs, ... }: {
+        system.stateVersion = "25.11";
+        nixpkgs.config.allowUnfree = true;
+        time.timeZone = "Europe/Minsk";
+        programs.silentSDDM = {
+          enable = false;
+          theme = "rei";
+        };
+        services.displayManager.sddm.theme = "breeze";
+        home-manager.useGlobalPkgs = true;
+        home-manager.useUserPackages = true;
+        home-manager.backupFileExtension = "bak";
+        home-manager.users.hikari = { pkgs, ... }: {
+          home.stateVersion = "25.11";
+          programs.home-manager.enable = true;
+          imports = [ ./home-manager/home-manager.nix ];
+        };
+      })
+    ];
+  };
+} // flake-utils.lib.eachDefaultSystem (system:
   let
     pkgs = import nixpkgs { inherit system; config.allowUnfree = true; };
   in
@@ -58,4 +63,58 @@
       shellHook = "echo 'nix fmt | statix check . | deadnix .'";
     };
   });
+<<<<<<< Updated upstream
+=======
+=======
+let
+system = "x86_64-linux";
+pkgs = import nixpkgs { inherit system; config.allowUnfree = true; };
+in
+{
+nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+inherit system;
+modules = [
+./hardware-configuration.nix
+./modules/boot.nix
+./modules/networking.nix
+./modules/users.nix
+./modules/keyboard.nix
+./modules/nvidia.nix
+./modules/hyprland.nix
+./modules/audio.nix
+./modules/packages.nix
+./sddm/SilentSDDM/nix/module.nix
+home-manager.nixosModules.home-manager
+({ config, pkgs, ... }: {
+system.stateVersion = "25.11";
+nixpkgs.config.allowUnfree = true;
+time.timeZone = "Europe/Minsk";
+programs.silentSDDM = {
+enable = true;
+theme = "rei";
+};
+home-manager.useGlobalPkgs = true;
+home-manager.useUserPackages = true;
+home-manager.backupFileExtension = "bak";
+home-manager.users.hikari = { pkgs, ... }: {
+home.stateVersion = "25.11";
+programs.home-manager.enable = true;
+imports = [ ./home-manager/home-manager.nix ];
+};
+})
+];
+};
+} // flake-utils.lib.eachDefaultSystem (system:
+let
+pkgs = import nixpkgs { inherit system; config.allowUnfree = true; };
+in
+{
+formatter = pkgs.nixpkgs-fmt;
+devShells.default = pkgs.mkShell {
+buildInputs = with pkgs; [ nixpkgs-fmt statix deadnix git ];
+shellHook = "echo 'nix fmt | statix check . | deadnix .'";
+};
+});
+>>>>>>> refs/remotes/origin/main
+>>>>>>> Stashed changes
 }
