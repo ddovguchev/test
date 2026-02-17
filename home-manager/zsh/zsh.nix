74549ff8d093a1ee73176b8b0b7b3bdb78b92a68
Dzmitriy Dovguchev <ddovguchev@gmail.com>
2026-02-17 13:34:14 +0300

    2234234525

--- home-manager/zsh/zsh.nix
+++ home-manager/zsh/zsh.nix
@@ -16,6 +16,9 @@
     };
     initContent = ''
       setopt PROMPT_SUBST
+      setopt HIST_IGNORE_ALL_DUPS
+      setopt HIST_SAVE_NO_DUPS
+      setopt HIST_EXPIRE_DUPS_FIRST
       PROMPT='%F{blue}%n@%m%f:%F{green}%~%f$ '
       ___MY_VMOPTIONS_SHELL_FILE="''${HOME}/.jetbrains.vmoptions.sh"; if [ -f "''${___MY_VMOPTIONS_SHELL_FILE}" ]; then . "''${___MY_VMOPTIONS_SHELL_FILE}"; fi
     '';
