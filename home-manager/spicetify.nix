771fe394f9a8358cea36560dc0d33a1811b25389
Dzmitriy Dovguchev <ddovguchev@gmail.com>
2026-02-17 21:47:19 +0300

    2234234525

--- home-manager/spicetify.nix
+++ home-manager/spicetify.nix
@@ -7,4 +7,48 @@ in
     enable = true;
     enabledCustomApps = with spicePkgs.apps; [ marketplace ];
   };
+
+  home.activation.setSpicetifyMarketplaceTheme = inputs.home-manager.lib.hm.dag.entryAfter [ "writeBoundary" ] ''
+    cfg="$HOME/.config/spicetify/config-xpui.ini"
+    mkdir -p "$HOME/.config/spicetify"
+
+    if [ ! -f "$cfg" ]; then
+      cat > "$cfg" <<'EOF'
+[Setting]
+current_theme = marketplace
+EOF
+      exit 0
+    fi
+
+    if grep -q '^\[Setting\]' "$cfg"; then
+      if grep -q '^current_theme[[:space:]]*=' "$cfg"; then
+        sed -i 's/^current_theme[[:space:]]*=.*/current_theme = marketplace/' "$cfg"
+      else
+        awk '
+          BEGIN { inserted = 0 }
+          /^\[Setting\]/ {
+            print $0
+            print "current_theme = marketplace"
+            inserted = 1
+            next
+          }
+          { print $0 }
+          END {
+            if (inserted == 0) {
+              print ""
+              print "[Setting]"
+              print "current_theme = marketplace"
+            }
+          }
+        ' "$cfg" > "$cfg.tmp"
+        mv "$cfg.tmp" "$cfg"
+      fi
+    else
+      cat >> "$cfg" <<'EOF'
+
+[Setting]
+current_theme = marketplace
+EOF
+    fi
+  '';
 }
