0a5a9920a7563bf29bcec811fdd562a8702710df
Dzmitriy Dovguchev <ddovguchev@gmail.com>
2026-02-17 16:50:26 +0300

    2234234525

--- modules/nvidia.nix
+++ modules/nvidia.nix
@@ -1,14 +1,19 @@
 { config, pkgs, lib, ... }:
 {
   boot.kernelParams = [ "nvidia_drm.modeset=1" ];
+  boot.blacklistedKernelModules = [ "nouveau" ];
+  boot.initrd.kernelModules = [ "nvidia" "nvidia_modeset" "nvidia_uvm" "nvidia_drm" ];
+  boot.kernelModules = [ "nvidia" "nvidia_modeset" "nvidia_uvm" "nvidia_drm" ];
+
+  hardware.graphics.enable = true;
   services.xserver.enable = true;
   services.xserver.videoDrivers = [ "nvidia" ];
   hardware.nvidia = {
     modesetting.enable = lib.mkDefault true;
     # Proprietary kernel module is usually the most reliable on desktop NVIDIA.
     open = lib.mkDefault false;
-    # Use stable branch to avoid breakage after kernel updates.
-    package = lib.mkDefault config.boot.kernelPackages.nvidiaPackages.stable;
+    # Use production branch for maximum compatibility.
+    package = lib.mkDefault config.boot.kernelPackages.nvidiaPackages.production;
     powerManagement.enable = lib.mkDefault true;
     nvidiaSettings = lib.mkDefault true;
   };
