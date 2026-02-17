ea052a198b7870ea30eae0005ee256dba84e43fc
Dzmitriy Dovguchev <ddovguchev@gmail.com>
2026-02-17 20:40:32 +0300

    2234234525

--- modules/sddm-theme.nix
+++ modules/sddm-theme.nix
@@ -25,16 +25,12 @@ let
       cp "$src/configs/silvia.conf" "$base/configs/silvia.conf"
       cp "$src/backgrounds/rei.png" "$base/backgrounds/rei.png"
       cp "$src/backgrounds/silvia.png" "$base/backgrounds/silvia.png"
+      cp "$src/backgrounds/rei.mp4" "$base/backgrounds/rei.mp4"
+      cp "$src/backgrounds/silvia.mp4" "$base/backgrounds/silvia.mp4"
 
       # Keep the shipped default configuration aligned with our minimal set.
       substituteInPlace "$base/metadata.desktop" \
         --replace-warn "ConfigFile=configs/default.conf" "ConfigFile=configs/rei.conf"
-
-      # Avoid black screen issues from mp4 backgrounds in SDDM on NVIDIA.
-      substituteInPlace "$base/configs/rei.conf" \
-        --replace-warn "rei.mp4" "rei.png"
-      substituteInPlace "$base/configs/silvia.conf" \
-        --replace-warn "silvia.mp4" "silvia.png"
 
       runHook postInstall
     '';
@@ -60,6 +56,18 @@ in
         InputMethod = "qtvirtualkeyboard";
         GreeterEnvironment = "QML2_IMPORT_PATH=${silentTheme}${themeDir}/components/,QT_IM_MODULE=qtvirtualkeyboard";
       };
+      Wayland = {
+        CompositorCommand = "${pkgs.weston}/bin/weston --shell=kiosk -c /etc/sddm/weston.ini";
+      };
     };
   };
+
+  environment.etc."sddm/weston.ini".text = ''
+    [core]
+    idle-time=0
+
+    [output]
+    name=DP-1
+    mode=2560x1080@200
+  '';
 }
