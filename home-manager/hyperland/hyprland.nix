b1796f529bbfb09b81550d7702c36e3f7a49ff29
Dzmitriy Dovguchev <ddovguchev@gmail.com>
2026-02-17 03:27:40 +0300

    2234234525

--- home-manager/hyperland/hyprland.nix
+++ home-manager/hyperland/hyprland.nix
@@ -5,6 +5,10 @@
     settings = {
       "$mod" = "SUPER";
       monitor = "DP-4,2560x1080@200,0x0,1";
+      general = {
+        gaps_in = 3;
+        gaps_out = 5;
+      };
       input = {
         kb_layout = "us,ru";
         kb_options = "grp:alt_shift_toggle";
