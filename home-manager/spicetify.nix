13c4a2df96b250363c76338da587978c6ba550a9
Dzmitriy Dovguchev <ddovguchev@gmail.com>
2026-02-17 21:53:28 +0300

    2234234525

--- home-manager/spicetify.nix
+++ home-manager/spicetify.nix
@@ -5,9 +5,11 @@ in
 {
   programs.spicetify = {
     enable = true;
+    theme = spicePkgs.themes.catppuccin;
+    colorScheme = "mocha";
     enabledCustomApps = with spicePkgs.apps; [ marketplace ];
-    enabledExtensions = lib.optionals (spicePkgs.extensions ? marketplace) [
-      spicePkgs.extensions.marketplace
-    ];
+    enabledExtensions =
+      (lib.optionals (spicePkgs.extensions ? adblock) [ spicePkgs.extensions.adblock ])
+      ++ (lib.optionals (spicePkgs.extensions ? marketplace) [ spicePkgs.extensions.marketplace ]);
   };
 }
