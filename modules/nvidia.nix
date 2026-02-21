7056509db031a6db4b22291ff587a358fcb368d1
Your Name <you@example.com>
2026-02-21 04:10:03 +0300

    123

--- modules/nvidia.nix
+++ modules/nvidia.nix
@@ -1,28 +1,24 @@
 { config, pkgs, lib, ... }:
 {
-  boot.kernelParams = [
-    "nvidia_drm.modeset=1"
-    "video=DP-1:2560x1080@200"
-  ];
   boot.blacklistedKernelModules = [ "nouveau" ];
-  boot.initrd.kernelModules = [ "nvidia" "nvidia_modeset" "nvidia_uvm" "nvidia_drm" ];
-  boot.kernelModules = [ "nvidia" "nvidia_modeset" "nvidia_uvm" "nvidia_drm" ];
 
-  hardware.graphics.enable = true;
-  hardware.graphics.enable32Bit = true;
-  services.xserver.enable = true;
   services.xserver.videoDrivers = [ "nvidia" ];
-  hardware.nvidia = {
-    modesetting.enable = lib.mkDefault true;
-    # RTX 5060 Ti требует открытые модули ядра (open kernel modules)
-    open = true;  # ОБЯЗАТЕЛЬНО для RTX 5060 Ti (Blackwell)
-    powerManagement.enable = lib.mkDefault true;
-    nvidiaSettings = lib.mkDefault true;
+
+  hardware.graphics = {
+    enable = true;
+    enable32Bit = true;
   };
-  services.power-profiles-daemon.enable = lib.mkDefault true;
-  powerManagement.cpuFreqGovernor = lib.mkDefault "schedutil";
+
+  hardware.nvidia = {
+    open = false;
+    modesetting.enable = true;
+    powerManagement.enable = true;
+    nvidiaSettings = true;
+  };
+
   environment.sessionVariables = {
-    WLR_NO_HARDWARE_CURSORS = "1";
     LIBVA_DRIVER_NAME = "nvidia";
+    __GLX_VENDOR_LIBRARY_NAME = "nvidia";
+    NVD_BACKEND = "direct";
   };
 }
--- modules/packages.nix
+++ modules/packages.nix
@@ -1,97 +1,75 @@
 { config, pkgs, lib, ... }:
+
 let
-  xwaylandVideoBridgePkg =
-    if lib.hasAttrByPath [ "plasma6Packages" "xwaylandvideobridge" ] pkgs then
-      [ pkgs.plasma6Packages.xwaylandvideobridge ]
-    else if lib.hasAttrByPath [ "kdePackages" "xwaylandvideobridge" ] pkgs then
-      [ pkgs.kdePackages.xwaylandvideobridge ]
-    else
-      [ ];
-  cursorPkg =
-    if lib.hasAttrByPath [ "code-cursor" ] pkgs then
-      [ pkgs.code-cursor ]
-    else
-      [ ];
-  spicetifyPkg =
-    if lib.hasAttrByPath [ "spicetify-cli" ] pkgs then
-      [ pkgs."spicetify-cli" ]
-    else if lib.hasAttrByPath [ "spicetify" ] pkgs then
-      [ pkgs.spicetify ]
-    else
-      [ ];
+  firstAttr = paths:
+    let
+      xs = builtins.filter (p: lib.hasAttrByPath p pkgs) paths;
+    in
+      if xs == [] then null else lib.getAttrFromPath (builtins.head xs) pkgs;
+
+  xwaylandVideoBridge = firstAttr [
+    [ "plasma6Packages" "xwaylandvideobridge" ]
+    [ "kdePackages" "xwaylandvideobridge" ]
+  ];
+
+  codeCursor = firstAttr [
+    [ "code-cursor" ]
+  ];
+
+  spicetify = firstAttr [
+    [ "spicetify-cli" ]
+    [ "spicetify" ]
+  ];
+
   vesktopWrapped = lib.hiPrio (pkgs.writeShellScriptBin "vesktop" ''
     exec ${pkgs.vesktop}/bin/vesktop \
       --enable-features=UseOzonePlatform,WebRTCPipeWireCapturer \
       --ozone-platform=wayland \
-      --disable-gpu-compositing \
-      --disable-gpu \
       "$@"
   '');
 in
 {
   programs.steam = {
     enable = true;
+    gamescopeSessions.enable = true;
   };
-  environment.systemPackages = (with pkgs; [
-    git
-    curl
-    wget
-    unzip
-    nodejs_22
-    nodePackages.typescript
-    ripgrep
-    pciutils
-    nixpkgs-fmt
-    kubectl
-    k9s
-    helm
-    docker
-    docker-compose
-    ansible
-    tmux
-    neovim
-    jetbrains.idea
-    kitty
-    ranger
-    fastfetch
-    htop
-    firefox
-    spotify
-    blender
-    qemu
-    terraform
-    terragrunt
-    go
-    gcc
-    insomnia
-    vesktop
-    obs-studio
-    steam
-    burpsuite
-    metasploit
-    wireguard-tools
-    wireshark
-    teams-for-linux
-    telegram-desktop
-    gns3-gui
-    hyprlock
-    grim
-    slurp
-    wf-recorder
-    wl-screenrec
-    pulseaudio
-    pavucontrol
-    ffmpeg
-    swww
-    astal.gjs
-    astal.astal3
-    astal.io
-    astal.wireplumber
-    astal.notifd
-  ]) ++ [ vesktopWrapped ] ++ xwaylandVideoBridgePkg ++ cursorPkg ++ spicetifyPkg;
-  virtualisation.docker.enable = lib.mkDefault true;
-  virtualisation.docker.rootless = {
-    enable = lib.mkDefault false;
-    setSocketVariable = lib.mkDefault true;
+  programs.gamemode.enable = true;
+
+  environment.systemPackages =
+    (with pkgs; [
+      # base
+      git curl wget unzip ripgrep pciutils htop fastfetch nixpkgs-fmt
+
+      # dev
+      nodejs_22 nodePackages.typescript go gcc terraform terragrunt
+      kubectl k9s helm ansible tmux neovim
+
+      # containers/virt
+      docker docker-compose qemu
+
+      # apps
+      jetbrains.idea kitty ranger firefox spotify blender insomnia obs-studio
+      steam burpsuite metasploit mangohub
+
+      # network
+      wireguard-tools wireshark gns3-gui teams-for-linux telegram-desktop
+
+      # wayland
+      hyprlock grim slurp wf-recorder wl-screenrec pavucontrol ffmpeg swww
+
+      # astal
+      astal.gjs astal.astal3 astal.io astal.wireplumber astal.notifd
+    ])
+    ++ [ vesktopWrapped ]
+    ++ lib.optional (xwaylandVideoBridge != null) xwaylandVideoBridge
+    ++ lib.optional (codeCursor != null) codeCursor
+    ++ lib.optional (spicetify != null) spicetify;
+
+  virtualisation.docker = {
+    enable = lib.mkDefault true;
+    rootless = {
+      enable = lib.mkDefault false;
+      setSocketVariable = lib.mkDefault true;
+    };
   };
 }
