69ff2239bec4088d6d3c05ffd149132d0a828e21
Your Name <you@example.com>
2026-02-21 13:59:58 +0300

    123

--- home-manager/ranger/ranger.nix
+++ home-manager/ranger/ranger.nix
@@ -1,10 +1,33 @@
 { config, pkgs, ... }:
+
 {
   programs.ranger = {
     enable = true;
+
     settings = {
-      defaultEditor = "nvim";
-      defaultViewer = "nvim";
+      preview_images = true;
+      use_preview_script = true;
     };
   };
+
+  environment.etc."ranger/scope.sh".text = ''
+    #!/usr/bin/env bash
+
+    FILE="$1"
+    EXT="${FILE##*.}"
+
+    case "$EXT" in
+      pdf)
+        pdftoppm -jpeg -f 1 -singlefile "$FILE" "/tmp/ranger_preview"
+        echo "/tmp/ranger_preview.jpg"
+        exit 0
+        ;;
+      jpg|jpeg|png|webp|bmp|gif)
+        echo "$FILE"
+        exit 0
+        ;;
+    esac
+
+    exit 1
+  '';
 }
--- home-manager/spicetify.nix
+++ home-manager/spicetify.nix
@@ -1,30 +1,26 @@
 { pkgs, inputs, lib, ... }:
+
 let
   spicePkgs = inputs.spicetify-nix.legacyPackages.${pkgs.stdenv.hostPlatform.system};
-  useTextTheme = spicePkgs.themes ? text;
-  selectedTheme = if useTextTheme then spicePkgs.themes.text else spicePkgs.themes.catppuccin;
-  darkThemerExtension =
-    if spicePkgs.extensions ? darkThemer then
-      [ spicePkgs.extensions.darkThemer ]
-    else if spicePkgs.extensions ? darkthemer then
-      [ spicePkgs.extensions.darkthemer ]
-    else if lib.hasAttr "dark-themer" spicePkgs.extensions then
-      [ (builtins.getAttr "dark-themer" spicePkgs.extensions) ]
-    else
-      [ ];
+
+  theme =
+    if spicePkgs.themes ? text
+    then spicePkgs.themes.text
+    else spicePkgs.themes.catppuccin;
+
+  ext = spicePkgs.extensions;
 in
 {
-  programs.spicetify =
-    {
-      enable = true;
-      theme = selectedTheme;
-      enabledCustomApps = with spicePkgs.apps; [ marketplace ];
-      enabledExtensions =
-        darkThemerExtension
-        ++ (lib.optionals (spicePkgs.extensions ? adblock) [ spicePkgs.extensions.adblock ])
-        ++ (lib.optionals (spicePkgs.extensions ? marketplace) [ spicePkgs.extensions.marketplace ]);
-    }
-    // (lib.optionalAttrs (!useTextTheme) {
-      colorScheme = "mocha";
-    });
+  programs.spicetify = {
+    enable = true;
+
+    theme = theme;
+    enabledCustomApps = [ spicePkgs.apps.marketplace ];
+
+    enabledExtensions =
+      (lib.optionals (ext ? adblock) [ ext.adblock ])
+      ++ (lib.optionals (ext ? marketplace) [ ext.marketplace ]);
+
+    colorScheme = lib.mkIf (!(spicePkgs.themes ? text)) "mocha";
+  };
 }
