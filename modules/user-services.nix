faa5a3e3bc886c54c64cb56ec1e84fe840d725bf
Your Name <you@example.com>
2026-02-21 05:10:12 +0300

    123

--- modules/user-services.nix
+++ modules/user-services.nix
@@ -1,28 +1,27 @@
 { config, pkgs, ... }:
+
 {
   systemd.user.services = {
     ags = {
-      Unit = {
-        Description = "Aylur's GTK Shell";
-        After = [ "graphical-session.target" ];
-      };
+      description = "Aylur's GTK Shell";
+      after = [ "graphical-session.target" ];
+      wantedBy = [ "default.target" ];
+
       serviceConfig = {
-        ExecStart = "/run/current-system/sw/bin/ags";
+        ExecStart = "${pkgs.ags}/bin/ags";
         Restart = "on-failure";
       };
-      wantedBy = [ "default.target" ];
     };
 
-    swww-daemon = {
-      Unit = {
-        Description = "Swww Wallpaper Daemon";
-        After = [ "graphical-session.target" ];
-      };
+    swww = {
+      description = "swww wallpaper daemon";
+      after = [ "graphical-session.target" ];
+      wantedBy = [ "default.target" ];
+
       serviceConfig = {
-        ExecStart = "/run/current-system/sw/bin/swww-daemon";
+        ExecStart = "${pkgs.swww}/bin/swww-daemon";
         Restart = "on-failure";
       };
-      wantedBy = [ "default.target" ];
     };
   };
 }
